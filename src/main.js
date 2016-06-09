77cd2d04a5dbedd5c3cd3f321df6a581fb9b9397
le0pard <leopard.not.a@gmail.com>
2016-06-10 01:43:56 +0300

    screen

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ let win
 const createWindow = () => {
   // Create the browser window.
   win = new BrowserWindow({
-    width:  800,
-    height: 600
+    width:  1024,
+    height: 768
   })
 
   // and load the index.html of the app.
