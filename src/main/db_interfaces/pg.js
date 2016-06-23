afd0234d1560594c236a53afe8b297e1456ea614
Alexey Vasiliev <leopard.not.a@gmail.com>
2016-06-23 15:11:22 +0300

    select databases and all kind of tables

--- src/main/db_interfaces/pg.js
+++ src/main/db_interfaces/pg.js
@@ -1,19 +1,23 @@
 import Promise from 'bluebird'
 import pg from 'pg'
 import ipcChannels from 'constants/ipc_channels'
+import _groupBy from 'lodash/collection/groupBy'
 
 let pgClients = {}
 
 const PGInterface = {
 
   connectToDatabase: ({database, ipcRequestId, event}) => {
+    const pgHostname = database.socket || database.hostname || 'localhost'
+    const pgPort = database.port || 5432
+    const pgDatabase = database.database || 'postgres'
     if (!pgClients[database.id]) {
       pgClients[database.id] = new pg.Pool({
         user: database.username,
         password: database.password,
-        host: database.socket || database.hostname || 'localhost',
-        port: database.port || 5432,
-        database: database.database || 'postgres',
+        host: pgHostname,
+        port: pgPort,
+        database: pgDatabase,
         max: 10, // max number of clients in pool
         idleTimeoutMillis: 10000
       })
@@ -22,25 +26,33 @@ const PGInterface = {
     pgClients[database.id].connect().then((client) => {
       Promise.all([
         client.query('SELECT VERSION() as version'),
+        client.query('SELECT pg_database.datname as name FROM pg_database WHERE pg_database.datistemplate = $1 AND pg_database.datallowconn = $2', [false, true]),
         client.query('SELECT n.nspname AS name, pg_catalog.pg_get_userbyid(n.nspowner) AS owner, pg_catalog.array_to_string(n.nspacl, E\'\n\') AS access_privileges, pg_catalog.obj_description(n.oid, \'pg_namespace\') AS description FROM pg_catalog.pg_namespace n WHERE n.nspname !~ \'^pg_\' AND n.nspname <> \'information_schema\' ORDER BY 1')
       ]).then((results) => {
-        const [versionResult, schemasResult] = results
+        const [versionResult, databasesResult, schemasResult] = results
         const {version} = versionResult.rows[0]
+        const databasesRows = databasesResult.rows
         const schemasRows = schemasResult.rows
         if (schemasRows.length) {
           const tablesQueries = schemasRows.map((schema) => client.query('SELECT * FROM information_schema.tables WHERE table_schema = $1', [schema.name]))
           Promise.all(tablesQueries).then((tablesResults) => {
             const schemas = schemasRows.reduce((agg, schema, index) => {
-              agg.push({...schema, tables: tablesResults[index].rows})
+              const tables = _groupBy(tablesResults[index].rows, (table) => table.table_type)
+              agg.push({...schema,
+                tables: tables['BASE TABLE'] || [],
+                views: tables['VIEW'] || [],
+                ['foreign_tables']: tables['FOREIGN TABLE'] || [],
+                ['local_temporary_tables']: tables['LOCAL TEMPORARY'] || []
+              })
               return agg
             }, [])
-            event.sender.send(ipcChannels.IPC_SUCCESS_CHANNEL, {version, schemas, ipcRequestId})
+            PGInterface._returnSchemaWithDatabases({pgDatabase, version, databasesRows, schemas, ipcRequestId, event})
           }).catch((queryError) => {
             event.sender.send(ipcChannels.IPC_ERROR_CHANNEL, {error: queryError, ipcRequestId})
           }).finally(() => client.release())
         } else {
           client.release()
-          event.sender.send(ipcChannels.IPC_SUCCESS_CHANNEL, {version, schemas: [], ipcRequestId})
+          PGInterface._returnSchemaWithDatabases({pgDatabase, version, databasesRows, schemas: [], ipcRequestId, event})
         }
 
       }).catch((queryError) => {
@@ -50,6 +62,16 @@ const PGInterface = {
     }).catch((connectError) => {
       event.sender.send(ipcChannels.IPC_ERROR_CHANNEL, {error: connectError, ipcRequestId})
     })
+  },
+
+  _returnSchemaWithDatabases({pgDatabase, version, databasesRows, schemas, ipcRequestId, event}) {
+    const databases = databasesRows.map((database) => {
+      if (database.name === pgDatabase)
+        return {...database, schemas}
+      else
+        return database
+    })
+    event.sender.send(ipcChannels.IPC_SUCCESS_CHANNEL, {version, databases, ipcRequestId})
   },
 
   closeAllConnections: () => {
