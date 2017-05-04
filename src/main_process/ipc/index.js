d82ee1de2897434183b1c81d9e397629d058cf52
le0pard <leopard.not.a@gmail.com>
2017-05-05 00:52:28 +0300

    move driver

--- src/main_process/ipc/index.js
+++ src/main_process/ipc/index.js
@@ -12,16 +12,16 @@ const initErrorResponse = ({actionTypes, event, args}) => (error) => {
   event.sender.send(actionTypes.IPC_ERROR_CHANNEL, {ipcFailure, error});
 };
 
-const handleIpcRequest = ({actionTypes, event, args}) => {
+const handleIpcRequest = ({winID, actionTypes, event, args}) => {
   const handleSuccessResponse = initSuccessResponse({actionTypes, event, args});
   const handleErrorResponse = initErrorResponse({actionTypes, event, args});
-  driverResponse({args, handleSuccessResponse, handleErrorResponse});
+  driverResponse({args, winID, handleSuccessResponse, handleErrorResponse});
 };
 
 export const listenIpcChannels = (winID) => {
   const actionTypes = getIpcActionTypes(winID);
   ipcMain.on(actionTypes.IPC_CHANNEL, (event, args) => {
-    handleIpcRequest({actionTypes, event, args});
+    handleIpcRequest({winID, actionTypes, event, args});
   });
 };
 
